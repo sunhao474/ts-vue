630816cf04605c8935217e9b958acbee1882bfe4
root <root@DESKTOP-MN2IB84.localdomain>
2021-03-05 15:20:52 +0800

    dev

---
+++ mapLib/declare/com.d.ts
@@ -0,0 +1,16 @@
+interface Vue {
+  _amap: any;
+  id: any
+}
+
+interface convertMap {
+  [index: string]: Function;
+}
+
+interface Handler {
+  [index: string]: Function;
+}
+
+interface ComponentEvent {
+  [index: string]: AMap.EVENT;
+}
--- mapLib/declare/lib.d.ts
+++ mapLib/declare/lib.d.ts
@@ -1,18 +1,1 @@
 import "@amap/amap-jsapi-types";
-
-interface Vue {
-  _amap: any;
-  id: any
-}
-
-interface convertMap {
-  [index: string]: Function;
-}
-
-interface Handler {
-  [index: string]: Function;
-}
-
-interface ComponentEvent {
-  [index: string]: AMap.EVENT;
-}
